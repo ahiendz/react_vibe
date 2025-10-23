35f1de9261218a86dc1d12a6b6583b4f3c4c710e
ahiendz <lyanhhien18072011@gmail.com>
2025-10-24 01:02:02 +0700

    page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 👇 THÊM base: '/<repo-name>/' VÀO ĐÂY
 export default defineConfig({
   plugins: [react()],
+  base: '/react_vibe/', // ⚠️ THAY 'react_vibe' = TÊN REPO TRÊN GITHUB CỦA M
 })
